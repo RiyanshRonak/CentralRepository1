13448627964e50ce521ad10fd534ba151cb7de60
patil <patil@Shweta>
2022-11-16 11:10:10 +0530

    1St Commit.

---
+++ GitHubPrepare/src/ArithmaticOperation/AdditionPgm.java
@@ -0,0 +1,15 @@
+package ArithmaticOperation;
+
+public class AdditionPgm 
+{
+	public static void main(String args[])
+	{
+		int a=20;
+		int b=10;
+		int c;
+		c=a+b;
+		System.out.println("Addition of "+c);
+		
+	}
+
+}
